6adf48154488ecca3eb956424811aedfa75ab758
dawidkrol <dawidkrol7@gmail.com>
2022-02-21 21:16:19 +0100

    Dodaj pliki projektów.

---
+++ ToDoDatabase/dbo/Tables/Statuses.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Statuses]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(10) NOT NULL
+)
---
+++ ToDoDatabase/dbo/Tables/Tasks.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Tasks]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [CreatingDate] DATETIME NOT NULL, 
+    [Deadline] NCHAR(10) NULL, 
+    [UserId] NVARCHAR (128) NOT NULL, 
+    [Status] INT NOT NULL DEFAULT 1,
+
+)
